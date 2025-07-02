45205b4f2684235ba2adc61a129649d8d2a7ef62
Ryoko-Tanahashi <ry.tana01202004@gmail.com>
2025-07-02 23:25:17 +0900

    update tsp

--- lec5/my_solver.py
+++ lec5/my_solver.py
@@ -7,32 +7,54 @@ from common import print_tour, read_input
 def distance(city1, city2):
     return math.sqrt((city1[0] - city2[0]) ** 2 + (city1[1] - city2[1]) ** 2)
 
-def two_opt(cities:list, dist:list[list[int]], tour:list[int]):
-    # tourの中から１点Aを選ぶ。Aの次の点をBとする。Aから距離の近い√N点に対して、以下の操作を行う。
-    # Aから近い点をCとする。Cの次の点をDとする。AB+CDとAC+BDを比較して、AC+BDの方が短かったらBとCを入れ替える。
+def two_opt(dist:list[list[int]], tour:list[int]):
     N = len(tour)
-    n = int(N ** 0.5)
-    for a in tour:
-        # tourの最後の点の時はスタート地点を、それ以外の時は次の地点をbとする
-        if tour.index(a) == len(tour)-1:
-            b = tour[0]
-        else:
-            b = tour[tour.index(a)+1]
+    if N < 50:
+        n = N
+    else:
+        n = int(N ** 0.8)
+    # 現在の距離を計算 distを使う
+    current_distance = sum(dist[tour[i]][tour[(i + 1) % N]]
+                              for i in range(N))
+    
+    count = 0
+    
+    while True:
+        count += 1
+        print(count)
+        past_distance = current_distance
+        # tourの中から１点Aを選ぶ。Aの次の点をBとする。Aから距離の近い√N点（N<100の場合はN/2点）に対して、以下の操作を行う。
+        # Aから近い点をCとする。Cの次の点をDとする。AB+CDとAC+BDを比較して、AC+BDの方が短かったらBとCを入れ替える。
+        for a in tour:
+            aIndex = tour.index(a)
+            # tourの最後の点の時はスタート地点を、それ以外の時は次の地点をbとする
+            if aIndex == len(tour)-1:
+                bIndex = tour.index(tour[0])
+            else:
+                bIndex = tour.index(tour[aIndex+1])
 
-        # aに近い順番にソートして、その中から√N個のみ選択
-        close_cities = sorted(range(len(dist[a])), key=lambda city: dist[a][city])[1:n]
-        for c in close_cities:
-            new_tour = tour.copy()
-            new_tour[tour.index(b)] = c
-            new_tour[tour.index(c)] = b
-            # 近い点cとbを入れ替えた状態と現在の状態で距離を比較し、入れ替えた方が良い場合は入れ替える
-            current_distance = sum(distance(cities[tour[i]], cities[tour[(i + 1) % N]])
-                              for i in range(N))
-            new_distance = sum(distance(cities[new_tour[i]], cities[new_tour[(i + 1) % N]])
-                              for i in range(N))
-            if(new_distance < current_distance):
-                tour[tour.index(b)] = c
-                tour[tour.index(c)] = b
+            # aに近い順番にソートして、その中から√N個（N<100の場合はN/2個）のみ選択
+            close_cities = sorted(range(len(dist[a])), key=lambda city: dist[a][city])[1:n]
+            for c in close_cities:
+                cIndex = tour.index(c)
+                if cIndex == len(tour)-1:
+                    dIndex = tour.index(tour[0])
+                else:
+                    dIndex = tour.index(tour[cIndex+1])
+                new_tour = tour.copy()
+                new_tour[bIndex:tour.index(dIndex)] = reversed(tour[bIndex:tour.index(dIndex)])
+                # 近い点cとbを入れ替えた状態と現在の状態で距離を比較し、入れ替えた方が良い場合は入れ替える
+                new_distance = sum(dist[new_tour[i]][new_tour[(i + 1) % N]]
+                                for i in range(N))
+                if(new_distance < current_distance):
+                    tour = new_tour
+                    current_distance = new_distance
+
+        # あまり更新されなくなってきたら終了
+        if (past_distance - current_distance)/past_distance < 0.01:
+            break
+
+    print(current_distance, count)
 
     return tour
 
@@ -56,7 +78,7 @@ def solve(cities):
         tour.append(next_city)
         current_city = next_city
     
-    improved_tour = two_opt(cities, dist, tour)
+    improved_tour = two_opt(dist, tour)
     return improved_tour
 
 
